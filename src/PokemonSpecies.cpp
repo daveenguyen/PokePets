68aa97ff3a70e52a1620bc4b3f76a5c59564e329
Davee Nguyen <vibrantdavee@gmail.com>
2013-03-30 11:47:09 -0700

    Started PokemonSpecies class

---
+++ src/PokemonSpecies.cpp
@@ -0,0 +1,33 @@
+#include "PokemonSpecies.h"
+
+
+/**
+ *
+ */
+PokemonSpecies::PokemonSpecies(int dexNum) {
+    /* parse database file based on pokedex number
+       then set these variables */
+    _name         = ;
+    _types[2]     = ;
+    _abilities[2] = ;
+    _genderRatio  = ;
+    _baseStats[6] = ;
+
+}
+
+/*
+        string  getName();
+        Type    getType(int index); // or gettypes?
+        Type[]  getTypes();
+        Ability getAbilities(int index);
+        double  getGenderRatio();
+        int     getBaseStat(int index);
+        int[]   getBaseStats();
+
+    private:
+        string  _name;
+        Types   _types[2];
+        Ability _abilities[2];
+        double  _genderRatio;
+        int     _baseStats[6];
+*/
---
+++ src/PokemonSpecies.h
@@ -0,0 +1,46 @@
+#ifndef POKEMONSPECIES_H
+#define POKEMONSPECIES_H
+
+#include <string>
+
+class PokemonSpecies
+{
+    public:
+
+        Pokemon(int dexNum);
+
+        ~Pokemon();
+
+        string  getName();
+        Type    getType(int index); // or gettypes?
+        Type[]  getTypes();
+        Ability getAbilities(int index);
+        double  getGenderRatio();
+        int     getBaseStat(int index);
+        int[]   getBaseStats();
+
+    private:
+
+        string  _name;
+        Types   _types[2];
+        Ability _abilities[2];
+        double  _genderRatio;
+        int     _baseStats[6];
+
+        // training based
+        int    _baseExp;
+        /*
+        int    _onDeathEVs[6];
+        */
+        double _capRate;
+        int    _baseHappiness;
+        Growth _rate;
+
+        // wild held items
+        // possible for multiple items
+        // has item and rate for item
+        // example garbodor with 3 items
+
+};
+
+#endif // POKEMONSPECIES_H
