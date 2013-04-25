321130f75ffc72e96377135c12c433d580267198
Albert <albertv714@gmail.com>
2013-04-24 22:28:16 -0700

    started on in game dialogue

---
+++ src/gameDialogue.h
@@ -0,0 +1,26 @@
+#ifndef GAMEDIALOGUE
+#define GAMEDIALOGUE
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class gameDialogue
+{
+	public:
+
+	private:
+		string playName;
+		string rivalName;
+		string npcName[]={Prof Haney,Old Man,Trainer,PokeDrug Trainer,Mayor,Musician,Dojo Master};
+		string gymLeader[]={Brock,Misty,Lt. Serge,Erick,Koga,Sabrina,Blain,Mr. X};
+		string eliteFour[]={Dale,Davee,Albert,Angel};
+		string text[]={}
+
+
+};
+
+
+
+#endif
